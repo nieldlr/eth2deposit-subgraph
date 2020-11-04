e2dc09a3fc5f5423e30774dbd4cf0f3318ff8bb9
Niel de la Rouviere <nieldlr@gmail.com>
2020-11-04 20:56:36 +0200

    Initial commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,48 +3,40 @@ import {
   Eth2DepositContract,
   DepositEvent
 } from "../generated/Eth2DepositContract/Eth2DepositContract"
-import { ExampleEntity } from "../generated/schema"
+import { Depositor, Deposit, Aggregation } from "../generated/schema"
 
 export function handleDepositEvent(event: DepositEvent): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let aggregation = Aggregation.load("1")
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (aggregation == null) {
+    // If this is the first time, set defaults
+    aggregation = new Aggregation("1")
+    aggregation.totalDeposits = BigInt.fromI32(0)
+    aggregation.totalDepositors = BigInt.fromI32(0)
+    aggregation.totalAmountDeposited = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let depositor = Depositor.load(event.transaction.from.toHex())
+  if(depositor == null) {
+    depositor = new Depositor(event.transaction.from.toHex())
+    depositor.depositCount = BigInt.fromI32(0)
+    aggregation.totalDepositors = aggregation.totalDepositors.plus(BigInt.fromI32(1))
+  }
 
-  // Entity fields can be set based on event parameters
-  entity.pubkey = event.params.pubkey
-  entity.withdrawal_credentials = event.params.withdrawal_credentials
+  depositor.depositCount = depositor.depositCount.plus(BigInt.fromI32(1))
+  aggregation.totalDeposits = aggregation.totalDeposits.plus(BigInt.fromI32(1))
+
+  let deposit = Deposit.load(event.transaction.hash.toHex())
+  deposit = new Deposit(event.transaction.hash.toHex())
+
+  deposit.pubkey = event.params.pubkey
+  deposit.withdrawal_credentials = event.params.withdrawal_credentials
+  deposit.amount = BigInt.fromUnsignedBytes(event.params.amount)
+  deposit.depositor = event.transaction.from.toHex()
+  aggregation.totalAmountDeposited = aggregation.totalAmountDeposited.plus(deposit.amount)
 
   // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.get_deposit_count(...)
-  // - contract.get_deposit_root(...)
-  // - contract.supportsInterface(...)
+  deposit.save()
+  depositor.save()
+  aggregation.save()
 }
